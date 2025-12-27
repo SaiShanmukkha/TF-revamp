75599116c941d5b81a4df11913f961b8ab404e7b
Sai S <90093621+SaiShanmukkha@users.noreply.github.com>
2025-12-27 11:50:37 -0600

    initial commit

--- main.tf
+++ main.tf
@@ -16,9 +16,10 @@ data "aws_ami" "app_ami" {
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
-  instance_type = "t3.nano"
+  instance_type = var.instance_type
 
   tags = {
-    Name = "HelloWorld"
+    Name = "SaiShanmukkha_TF_Cloud_Instance"
+    Owner = "Sai Shanmukkha Surapaneni"
   }
 }
